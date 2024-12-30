80121c473f58b1d990a613412f1b91826191d979
roboticlabs <robotlabsnz@gmail.com>
2024-12-30 07:26:27 +0000

    Deploying to gh-pages from @ roboticlabs/roboticlabs.github.io@c5fcab6a1ee0bd0df9d45d957c4a293c6598bd85 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
