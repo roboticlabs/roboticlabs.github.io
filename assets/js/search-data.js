feed4802098c9682f919d60a34dce19ee6bd9fe8
rbtlabs <robotlabsnz@gmail.com>
2025-01-03 00:36:44 +0000

    Deploying to gh-pages from @ rbtlabs/rbtlabs.github.io@48e01bb89bfe7a04f0a35df32ae036cbac9b4507 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
+          title: "Repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
